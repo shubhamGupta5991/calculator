b0d574cd6f26c248c1434aeb21fa5756a44c91dd
Shubham Gupta <guptashubham5991@gmail.com>
2024-02-13 13:57:39 +0530

    Project done

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Calculator = () => {
+  const [inputVal,setInputVal] = useState('')
+  let [result,setResult] = useState('')
+
+function clear(){
+  setInputVal('');
+  setResult('')
+}
+function handleCalc(){
+  
+  setResult(inputVal===''?'Error':eval(inputVal))
+  // setResult()
+
+}
+
+ 
   return (
-    <div>Calculator</div>
+    <>
+    <h1>React Calculator</h1>
+    <div>
+      <input type="text" value={inputVal} readOnly/>
+      <div>{result}</div>
+    </div>
+    <div>
+      <div>
+        <button value={'7'} onClick={(e)=>setInputVal(inputVal+e.target.value)}>7</button>
+        <button value={'8'} onClick={(e)=>setInputVal(inputVal+e.target.value)}>8</button>
+        <button value={'9'} onClick={(e)=>setInputVal(inputVal+e.target.value)}>9</button>
+        <button value={'+'} onClick={(e)=>setInputVal(inputVal+e.target.value)}>+</button>
+      </div>
+      <div>
+        <button value={4} onClick={(e)=>setInputVal(inputVal+e.target.value)}>4</button>
+        <button value={5} onClick={(e)=>setInputVal(inputVal+e.target.value)}>5</button>
+        <button value={6} onClick={(e)=>setInputVal(inputVal+e.target.value)}>6</button>
+        <button value={'-'} onClick={(e)=>setInputVal(inputVal+e.target.value)}>-</button>
+      </div>
+      <div>
+        <button value={1} onClick={(e)=>setInputVal(inputVal+e.target.value)}>1</button>
+        <button value={2} onClick={(e)=>setInputVal(inputVal+e.target.value)}>2</button>
+        <button value={3} onClick={(e)=>setInputVal(inputVal+e.target.value)}>3</button>
+        <button value={'*'} onClick={(e)=>setInputVal(inputVal+e.target.value)}>*</button>
+      </div>
+      <div>
+        <button onClick={clear}>C</button>
+        <button value={0} onClick={(e)=>setInputVal(inputVal+e.target.value)}>0</button>
+        <button onClick={handleCalc}>=</button>
+        <button value={'/'} onClick={(e)=>setInputVal(inputVal+e.target.value)}>/</button>
+      </div>
+    </div>
+    </>
   )
 }
 
